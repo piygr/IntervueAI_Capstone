9902a1c32531be624471b1543fc4f3700f3e8057
piygr <grover1piyush@gmail.com>
2025-06-10 21:54:38 +0530

    transcribe agent added

--- agents/interview_agent.py
+++ agents/interview_agent.py
@@ -2,6 +2,8 @@ import logging
 
 from dotenv import load_dotenv
 import asyncio
+
+from livekit import rtc
 from livekit.agents import (
     Agent,
     AgentSession,
@@ -12,6 +14,11 @@ from livekit.agents import (
     cli,
     metrics,
     RoomInputOptions,
+    RoomOutputOptions,
+    RoomIO,
+    StopResponse,
+    llm,
+    utils
 )
 from livekit.agents.llm.chat_context import ChatContext, ChatMessage
 from livekit.plugins import (
@@ -29,7 +36,7 @@ import os
 load_dotenv(dotenv_path=".env.local")
 logger = logging.getLogger("voice-agent")
 
-llm = google.LLM(
+llm_model = google.LLM(
         model="gemini-2.0-flash",
     )
 stt = aws.STT()
@@ -79,7 +86,7 @@ Here is the candidate's resume:
             stt=stt,
             tts=tts,
             #turn_detection=EnglishModel,
-            llm=llm
+            llm=llm_model
         )
 
     async def on_enter(self):
@@ -155,10 +162,122 @@ async def entrypoint(ctx: JobContext):
 
     await session.start(room=ctx.room, agent=agent)
 
+
+
+class Transcriber(Agent):
+    def __init__(self, *, participant_identity: str):
+        super().__init__(
+            instructions="not-needed",
+            stt=stt
+        )
+        self.participant_identity = participant_identity
+
+    async def on_user_turn_completed(self, chat_ctx: llm.ChatContext, new_message: llm.ChatMessage):
+        user_transcript = new_message.text_content
+        logger.info(f"{self.participant_identity} -> {user_transcript}")
+        self.session.say("I am listening...")
+        raise StopResponse()
+    
+
+class MultiUserTranscriber:
+    def __init__(self, ctx: JobContext):
+        self.ctx = ctx
+        self._sessions: dict[str, AgentSession] = {}
+        self._tasks: set[asyncio.Task] = set()
+
+    def start(self):
+        self.ctx.room.on("participant_connected", self.on_participant_connected)
+        self.ctx.room.on("participant_disconnected", self.on_participant_disconnected)
+
+    async def aclose(self):
+        await utils.aio.cancel_and_wait(*self._tasks)
+
+        await asyncio.gather(*[self._close_session(session) for session in self._sessions.values()])
+
+        self.ctx.room.off("participant_connected", self.on_participant_connected)
+        self.ctx.room.off("participant_disconnected", self.on_participant_disconnected)
+
+    def on_participant_connected(self, participant: rtc.RemoteParticipant):
+        if participant.identity in self._sessions:
+            return
+
+        logger.info(f"starting session for {participant.identity}")
+        task = asyncio.create_task(self._start_session(participant))
+        self._tasks.add(task)
+
+        def on_task_done(task: asyncio.Task):
+            try:
+                self._sessions[participant.identity] = task.result()
+            finally:
+                self._tasks.discard(task)
+
+        task.add_done_callback(on_task_done)
+
+    def on_participant_disconnected(self, participant: rtc.RemoteParticipant):
+        if (session := self._sessions.pop(participant.identity)) is None:
+            return
+
+        logger.info(f"closing session for {participant.identity}")
+        task = asyncio.create_task(self._close_session(session))
+        self._tasks.add(task)
+        task.add_done_callback(lambda _: self._tasks.discard(task))
+
+    async def _start_session(self, participant: rtc.RemoteParticipant) -> AgentSession:
+        if participant.identity in self._sessions:
+            return self._sessions[participant.identity]
+
+        session = AgentSession(
+            vad=self.ctx.proc.userdata["vad"],
+            tts=tts
+        )
+        room_io = RoomIO(
+            agent_session=session,
+            room=self.ctx.room,
+            participant=participant,
+            input_options=RoomInputOptions(
+                # text input is not supported for multiple room participants
+                # if needed, register the text stream handler by yourself
+                # and route the text to different sessions based on the participant identity
+                text_enabled=False,
+            ),
+            output_options=RoomOutputOptions(
+                transcription_enabled=True,
+                audio_enabled=True,
+            ),
+        )
+        await room_io.start()
+        await session.start(
+            agent=Transcriber(
+                participant_identity=participant.identity
+            )
+        )
+        return session
+
+    async def _close_session(self, sess: AgentSession) -> None:
+        await sess.drain()
+        await sess.aclose()
+
+
+async def entrypoint2(ctx: JobContext):
+    transcriber = MultiUserTranscriber(ctx)
+    transcriber.start()
+
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    for participant in ctx.room.remote_participants.values():
+        # handle all existing participants
+        transcriber.on_participant_connected(participant)
+
+    async def cleanup():
+        await transcriber.aclose()
+
+    ctx.add_shutdown_callback(cleanup)
+    
+
+
 if __name__ == "__main__":
     cli.run_app(
         WorkerOptions(
-            entrypoint_fnc=entrypoint,
+            entrypoint_fnc=entrypoint2,
             prewarm_fnc=prewarm,
         ),
     )
