e8f8b4b2b385efba641e1619582ae01144001752
walnashgit <avikragarwal@gmail.com>
2025-06-15 14:41:59 +0530

    Turn detection and interruption

--- agents/interview_agent.py
+++ agents/interview_agent.py
@@ -1,4 +1,5 @@
 import logging
+from typing import AsyncIterable, Optional
 
 from dotenv import load_dotenv
 import asyncio
@@ -12,27 +13,49 @@ from livekit.agents import (
     cli,
     metrics,
     RoomInputOptions,
+    llm,
+    stt,
+    FunctionTool
 )
 from livekit.agents.llm.chat_context import ChatContext, ChatMessage
+from livekit.agents.llm import StopResponse
+from livekit.agents.stt.stt import SpeechEvent
+from livekit.agents.voice.agent import ModelSettings
 from livekit.plugins import (
     openai,
     silero,
     aws,
-    google
+    google,
+    noise_cancellation
 )
-#from livekit.plugins.turn_detector.english import EnglishModel
+from livekit import rtc
+from livekit.plugins.turn_detector.english import EnglishModel
+from livekit.agents import UserStateChangedEvent, AgentStateChangedEvent
 
 from utils.utils import fetch_session
+from utils import llm as local_llm
+from datetime import datetime
+from google import genai
 
 import os
 
 load_dotenv(dotenv_path=".env.local")
+api_key = os.getenv("GOOGLE_API_KEY")
+client = genai.Client(api_key=api_key)
+
 logger = logging.getLogger("voice-agent")
 
-llm = google.LLM(
+my_llm = google.LLM(
         model="gemini-2.0-flash",
     )
-stt = aws.STT()
+# my_llm = google.beta.realtime.RealtimeModel(
+#         model="gemini-2.0-flash-exp",
+#         voice="Puck",
+#         temperature=0.8,
+#         instructions="You are a helpful voice AI assistant",
+#     )
+
+my_stt = aws.STT()
 tts = aws.TTS(
     voice="Kajal",
     language="en-IN",
@@ -73,19 +96,70 @@ Here is the job description:
 Here is the candidate's resume:
 {resume}
 """
+        instructions1 = "You are a helpful voice AI assistant."
 
         super().__init__(
             instructions=instructions,
-            stt=stt,
-            tts=tts,
+            # stt=stt,
+            # tts=tts,
             #turn_detection=EnglishModel,
-            llm=llm
+            # llm=my_llm
         )
 
     async def on_enter(self):
         # The agent should be polite and greet the user when it joins :)
-        self.session.say("Hey, how are you doing today?")
+        # self.session.say("Hey, how are you doing today?")
+        handle = self.session.say("Hey, how are you doing today?")
+        handle.add_done_callback(lambda _: print("speech done"))
 
+    async def on_user_turn_completed(self, turn_ctx: ChatContext, new_message: ChatMessage) -> None:
+        logger.info("user turn completed...")
+        cur_time = datetime.now()
+        logger.info(f'chat ctx:...{self.session._chat_ctx.items}...')
+        logger.info(f'turn_ctx:----{turn_ctx.items}----')
+        print(f'time elapsed: {cur_time - time}')
+
+        user_transcript = new_message.text_content
+        logger.info(f"user text: {user_transcript}")
+
+        # prompt = f"Give me only a one sentence reply to these text: {user_transcript}"
+        # reply = await local_llm.call_llm_with_timeout(client=client, prompt=prompt)
+        # self.session.say(reply.strip())
+
+        # self.session.say("I am listening...")
+        # raise StopResponse()
+
+        if ("wait for a minute" in new_message.text_content):
+            print(f'ok waiting....')
+            # await self.session.interrupt()
+            turn_ctx.add_message(role="user", content=new_message.text_content)
+            await self.update_chat_ctx(turn_ctx)
+            await self.session.say("Ok I'll wait")
+            raise StopResponse()
+        # # callback before generating a reply after user turn committed
+        # if not new_message.text_content:
+        #     # for example, raise StopResponse to stop the agent from generating a reply
+        #     logger.info("ignore empty user turn")
+
+    async def stt_node(self, audio: AsyncIterable[rtc.AudioFrame], model_settings: ModelSettings):
+        logger.info("stt_node...")
+        return Agent.default.stt_node(self, audio, model_settings)
+    
+    async def llm_node(
+        self,
+        chat_ctx: llm.ChatContext,
+        tools: list[FunctionTool],
+        model_settings: ModelSettings
+    ) -> AsyncIterable[llm.ChatChunk]:
+        logger.info("llm_node...")
+        return Agent.default.llm_node(self, chat_ctx, tools, model_settings)
+
+    async def tts_node(
+        self, text: AsyncIterable[str], model_settings: ModelSettings
+    ) -> AsyncIterable[rtc.AudioFrame]:
+        logger.info("tts_node...")
+        return Agent.default.tts_node(self, text, model_settings)
+        
 
 def prewarm(proc: JobProcess):
     proc.userdata["vad"] = silero.VAD.load()
@@ -130,19 +204,57 @@ async def entrypoint(ctx: JobContext):
         jd_text = file.read()
 
     agent = Assistant(job_description=jd_text, resume=session_details.get("resume"))
+    # agent = Assistant(job_description="", resume="")
     
     #print("AWS-->", os.getenv("AWS_ACCESS_KEY_ID"), os.getenv("AWS_REGION"))
 
     session = AgentSession(
+        # turn_detection=EnglishModel(),
         vad=ctx.proc.userdata["vad"],
         # minimum delay for endpointing, used when turn detector believes the user is done with their turn
         min_endpointing_delay=0.5,
         # maximum delay for endpointing, used when turn detector does not believe the user is done with their turn
-        max_endpointing_delay=5.0
+        max_endpointing_delay=5.0,
+        allow_interruptions=True,
+        stt=my_stt,
+        tts=tts,
+        llm=my_llm
     )
 
     # Trigger the on_metrics_collected function when metrics are collected
     session.on("metrics_collected", on_metrics_collected)
+
+
+    @session.on("user_state_changed")
+    def on_user_state_changed(ev: UserStateChangedEvent):
+        if ev.new_state == "speaking":
+            logger.info("User started speaking")
+        elif ev.new_state == "listening":
+            logger.info("User stopped speaking")
+            global time
+            time = datetime.now()
+        elif ev.new_state == "away":
+            logger.info("User is not present (e.g. disconnected)")
+
+    @session.on("agent_state_changed")
+    def on_agent_state_changed(ev: AgentStateChangedEvent):
+        if ev.new_state == "initializing":
+            logger.info("Agent is starting up")
+        elif ev.new_state == "idle":
+            logger.info("Agent is ready but not processing")
+        elif ev.new_state == "listening":
+            logger.info("Agent is listening for user input")
+        elif ev.new_state == "thinking":
+            logger.info("Agent is processing user input and generating a response")
+            # session.interrupt()
+            # logger.info("Will take time to respond")
+
+        elif ev.new_state == "speaking":
+            logger.info("Agent started speaking")
+
+    @session.on("user_speech_committed")
+    def on_user_speech_committed(msg: llm.ChatMessage):
+        logger.debug(f'user speech commited... {msg}')
 
     try:
         logger.info("Waiting for participant to join (max 30s)...")
@@ -153,7 +265,15 @@ async def entrypoint(ctx: JobContext):
         await ctx.close()
         return  # Exits the entrypoint, safely ends the subprocess
 
-    await session.start(room=ctx.room, agent=agent)
+    await session.start(
+        room=ctx.room, 
+        agent=agent,
+        room_input_options=RoomInputOptions(
+            # enable background voice & noise cancellation, powered by Krisp
+            # included at no additional cost with LiveKit Cloud
+            noise_cancellation=noise_cancellation.BVC(),
+            )
+        )
 
 if __name__ == "__main__":
     cli.run_app(
